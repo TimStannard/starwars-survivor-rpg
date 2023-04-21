34ad3485458057408fca62f5dbd4a3117d6c484d
TimStannard <stannard.tim@gmail.com>
2023-04-21 19:39:59 +1200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Hello</h1>
+    </>
+  )
+}
+
+export default App
